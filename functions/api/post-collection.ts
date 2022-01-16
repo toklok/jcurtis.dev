a7e6ed48f29ef2a8966bb28a739b43c749b06b06
Joseph Curtis <joecurt85@gmail.com>
2022-01-16 15:37:00 -0500

    Update collectionByHandle
    Update post-collection.ts

--- functions/api/post-collection.ts
+++ functions/api/post-collection.ts
@@ -2,34 +2,43 @@ export async function onRequestPost({ env, query }) {
   const collectionQuery = `query GetCollection($handle: String!) {
     collectionByHandle(handle: $handle) {
       description
-      products(first: 5) {
+      products(first: 30) {
         edges {
           node {
             description
-            images(first: 5) {
+            images(first: 30) {
               edges {
                 node {
-                  url
+                  ... on Image {
+                    original: url
+                    thumbnail: url(transform: {maxWidth: 400, maxHeight: 400})
+                    retina: url(transform: {scale: 2})
+                    altText
+                  }
                 }
               }
             }
           }
         }
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
       }
     }
-  }
+  }  
   `;
 
   const collectionByHandle = await fetch(env.SHOPIFY_STORE, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json",
+      Accept: "application/json",
       "X-Shopify-Storefront-Access-Token": env.SHOPIFY_STOREFRONT_API,
     },
     body: JSON.stringify({
       query: collectionQuery,
-      variables: { "handle": "best-sellers" },
+      variables: { handle: "best-sellers" },
     }),
   })
     .then((res) => {
