cf42778f89ca4a67b7815335b20ee85428356e89
Joseph Curtis <joecurt85@gmail.com>
2022-07-30 09:02:55 -0400

    [#3] update Astro & deps
    Update astro and dependencies
    
    update img component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,11 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-import astroImagePlugin from "astro-imagetools/plugin";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import preact from '@astrojs/preact';
+import { astroImageTools } from "astro-imagetools";
 
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ['@astrojs/renderer-preact'],
-  buildOptions: {
-    sitemap: true,
-    site: 'https://jcurtis.dev/',
-  },
-  vite: {
-    plugins: [astroImagePlugin]
-  },
-});
+export default defineConfig({
+  output: 'static',
+  site: 'https://jcurtis.dev/',
+  integrations: [preact(), astroImageTools,sitemap()],
+})
